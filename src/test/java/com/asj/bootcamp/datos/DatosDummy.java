081938c212a494edb0abce440b56ffe7bdda0674
Brian Nieto <nietobrian435@gmail.com>
2023-03-21 03:08:16 -0300

    categoryRepositoryTest added

---
+++ src/test/java/com/asj/bootcamp/datos/DatosDummy.java
@@ -0,0 +1,12 @@
+package com.asj.bootcamp.datos;
+
+import com.asj.bootcamp.entity.Category;
+
+public class DatosDummy {
+
+    //CATEGORIAS
+    public static Category getCategorySMG(){
+        return new Category(1,"SMG");
+    }
+
+}
