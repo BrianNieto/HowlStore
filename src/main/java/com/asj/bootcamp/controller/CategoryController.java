e45833a016a5978980223d290ee9c811d7c3985d
Brian Nieto <nietobrian435@gmail.com>
2023-03-23 12:23:20 -0300

    modified cross origin

--- src/main/java/com/asj/bootcamp/controller/CategoryController.java
+++ src/main/java/com/asj/bootcamp/controller/CategoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/categorias")
 public class CategoryController {
--- src/main/java/com/asj/bootcamp/controller/ItemController.java
+++ src/main/java/com/asj/bootcamp/controller/ItemController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/items")
 public class ItemController {
